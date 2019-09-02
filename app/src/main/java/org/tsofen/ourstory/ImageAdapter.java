36c84d963fec6af650d767799f089e70c0e30654
Orwah Watad <orwah_wattad@hotmail.com>
2019-09-02 13:54:39 +0300

    first pull

---
+++ app/src/main/java/org/tsofen/ourstory/ImageAdapter.java
@@ -0,0 +1,56 @@
+package org.tsofen.ourstory;
+
+import android.content.Context;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ourstory.R;
+
+import java.util.List;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
+    private List<ImgItem> images;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context=parent.getContext();
+        LayoutInflater inflater= LayoutInflater.from(context);
+
+        View imgView = inflater.inflate((R.layout.memory_img_item,parent,false);
+
+        ViewHolder viewHolder = new ViewHolder(imgView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageAdapter.ViewHolder holder, int position) {
+        holder.imgView.setImageResource(images.get(position).getImg());
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ImageView imgView;
+
+
+        public ViewHolder(View itemView){
+            super(itemView);
+
+            imgView=(ImageView) itemView.findViewById(R.id.memory_img);
+        }
+    }
+    public ImageAdapter(List<ImgItem> images){
+        this.images=images;
+    }
+
+}
---
+++ app/src/main/java/org/tsofen/ourstory/ImgItem.java
@@ -0,0 +1,29 @@
+package org.tsofen.ourstory;
+
+import java.util.ArrayList;
+
+public class ImgItem {
+    String imgName;
+    Integer img;
+
+    public ImgItem(String name,Integer img){
+        this.imgName=name;
+        this.img=img;
+    }
+
+    public String getImgName(){
+        return imgName;
+    }
+
+    public void setImgName(String name){
+        this.imgName=name;
+    }
+
+    public Integer getImg() {
+        return img;
+    }
+
+    public void setImg(Integer img) {
+        this.img = img;
+    }
+}
